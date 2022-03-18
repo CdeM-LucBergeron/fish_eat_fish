0c19a39b090ac4aab483707464cc80447b64b1d2
Luc Bergeron <psyluche@gmail.com>
2022-03-18 13:35:02 -0400

    Added two animation for flip and non-flip.

--- player.py
+++ player.py
@@ -1,8 +1,39 @@
+from enum import Enum
+
 from fish_animation import FishAnimation
 
-class Player(FishAnimation):
-    def __init__(self, spritesheet_path):
-        super().__init__(spritesheet_path)
 
-        self.scale = 0.5
+class Direction(Enum):
+    LEFT = 0
+    RIGHT = 1
+
+class Player:
+    MOVEMENT_SPEED = 2.0
+    
+    def __init__(self, spritesheet_path):
+        #super().__init__(spritesheet_path)
+        self.left_animation = FishAnimation(spritesheet_path)
+        self.right_animation = FishAnimation(spritesheet_path, flip=True)
+        self.current_animation = None
+
+        self.face_left = True
+        if self.face_left:
+            self.current_animation = self.left_animation
+        else:
+            self.current_animation = self.right_animation
+
+    def draw(self):
+        self.current_animation.draw()
+
+    def update(self, delta_time):
+        self.current_animation.on_update(delta_time)
+
+    def change_direction(self):
+        self.face_left = not self.face_left
+        if self.face_left:
+            self.current_animation = self.left_animation
+        else:
+            self.current_animation = self.right_animation        
+        
+
     
